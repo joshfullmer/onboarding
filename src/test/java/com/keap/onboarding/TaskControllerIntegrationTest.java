fc272b297149890b3a30b74aafedb628ae6b7283
Josh Fullmer <joshfullmer9@gmail.com>
2019-04-17 14:03:13 -0700

    TaskService unit testing

--- src/test/java/com/keap/onboarding/TaskControllerIntegrationTest.java
+++ src/test/java/com/keap/onboarding/TaskControllerIntegrationTest.java
@@ -1,10 +1,11 @@
 package com.keap.onboarding;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,12 +13,12 @@ import org.springframework.test.web.servlet.MockMvc;
 @WebMvcTest(TaskController.class)
 public class TaskControllerIntegrationTest {
 
-  @Autowired
-  private MockMvc mvc;
+  MockMvc mvc;
 
   @Test
-  public void getTaskListGivenContactId() {
-
+  public void getTaskListGivenContactId() throws Exception{
+    mvc.perform(get("/contact/4/task"))
+        .andExpect(status().isOk());
   }
 
 }
